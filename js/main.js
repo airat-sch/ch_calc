a9aac0710055e26878043bea5014f78db5fa50e6
airat-sch <44980440+airat-sch@users.noreply.github.com>
2021-07-16 15:26:50 +0500

    remove disabled

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ window.onload = function () {
 
 function calculate() {
 	let c_ch_price = parseFloat(document.getElementById("c_ch_price").value.replace(/,/, '.')); // Стоимость в Китае Руб.
-	let c_ag_delivery = parseFloat(document.getElementById("c_ag_delivery").value); //Доставка до Агента
-	let c_log_price = parseFloat(document.getElementById("c_log_price").value); //Стоимость логистики
-	let c_outlay = parseFloat(document.getElementById("c_outlay").value); //Затраты на подготовку к отправке (коробка,бирка, бумага, подарок, веревочка)
-	let c_wb_transit = parseFloat(document.getElementById("c_wb_transit").value); //Транзит на WB
-	let c_receipt = parseFloat(document.getElementById("c_receipt").value); //Приемка
-	let c_storage = parseFloat(document.getElementById("c_storage").value); //Стоимость хранения (исходя из оборач. 90)
-	let c_pokat = parseFloat(document.getElementById("c_pokat").value); //Покатушки (исх. Из выкупа 50%)
+	let c_ag_delivery = parseFloat(document.getElementById("c_ag_delivery").value.replace(/,/, '.')); //Доставка до Агента
+	let c_log_price = parseFloat(document.getElementById("c_log_price").value.replace(/,/, '.')); //Стоимость логистики
+	let c_outlay = parseFloat(document.getElementById("c_outlay").value.replace(/,/, '.')); //Затраты на подготовку к отправке (коробка,бирка, бумага, подарок, веревочка)
+	let c_wb_transit = parseFloat(document.getElementById("c_wb_transit").value.replace(/,/, '.')); //Транзит на WB
+	let c_receipt = parseFloat(document.getElementById("c_receipt").value.replace(/,/, '.')); //Приемка
+	let c_storage = parseFloat(document.getElementById("c_storage").value.replace(/,/, '.')); //Стоимость хранения (исходя из оборач. 90)
+	let c_pokat = parseFloat(document.getElementById("c_pokat").value.replace(/,/, '.')); //Покатушки (исх. Из выкупа 50%)
 	let c_rc = parseFloat(document.getElementById("c_rc").value.replace(/,/, '.')); //РЦ
 	let c_wb_commission = c_rc / 100 * 15; //Комиссия WB 15%
 	let c_rc_commission = c_rc / 100 * 7; // Налог 7% от РЦ
